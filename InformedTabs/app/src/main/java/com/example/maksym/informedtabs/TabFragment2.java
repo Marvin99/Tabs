7e6965e1cbb26f2108e9c6f6dbe47dd27a8254e3
Marvin99 <maksym.yablonskyi@gmail.com>
2016-11-23 13:15:30 +0200

    Tabs

---
+++ InformedTabs/app/src/main/java/com/example/maksym/informedtabs/TabFragment2.java
@@ -0,0 +1,19 @@
+package com.example.maksym.informedtabs;
+
+/**
+ * Created by Maksym on 21.11.2016.
+ */
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TabFragment2 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.tab_fragment_2, container, false);
+    }
+}
